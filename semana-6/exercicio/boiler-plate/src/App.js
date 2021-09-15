86387d663b768ab227ab6a917fa2323f22b27a48
allan-gilber <allangilber@yahoo.com.br>
2021-09-15 03:16:09 -0300

    Entrega dos exércicios faltando apenas a última parte.

--- semana-6/exercicio/boiler-plate/src/App.js
+++ semana-6/exercicio/boiler-plate/src/App.js
@@ -27,40 +27,112 @@ class App extends React.Component {
           completa: false
         },
         {
-          id: Date.now(), // Explicação abaixo
+          id: Date.now(), 
           texto: 'Texto da segunda tarefa',
-          completa: true // Indica se a tarefa está completa (true ou false)
+          completa: true
         }
       ],
+
       inputValue: '',
-      filtro: ''
+      filtro: '',
+
     }
 
-  componentDidUpdate() {
-
-  };
-
-  componentDidMount() {
-
-  };
-
   onChangeInput = (event) => {
+
+this.setState({ inputValue: event.target.value});
 
   }
 
   criaTarefa = () => {
 
+    const novoInput = {
+
+      id: Date.now(),
+      texto: this.state.inputValue,
+      completa: false
+
+    };
+
+    const spread = [...this.state.tarefas, novoInput]
+
+    this.setState({ tarefas: spread })
+    this.setState({inputValue: ''})
+
   }
 
   selectTarefa = (id) => {
+
+    const novasTarefas = this.state.tarefas.map(tarefa => {
+
+    if (id === tarefa.id ){
+
+      const tarefaSelecionada = {
+
+        ...tarefa,
+
+        completa: !tarefa.completa
+
+      }
+
+      return tarefaSelecionada
+
+    } else {
+
+      return tarefa
+
+    }
+
+  })
+
+    this.setState({ tarefas: novasTarefas })
 
   }
 
   onChangeFilter = (event) => {
 
+    this.setState({ filtro: event.target.value});
+
+  }
+
+  componentDidMount() {
+
+    const formString = window.localStorage.getItem("dadosArmazenados")
+    
+
+    if(formString){
+
+      const form = JSON.parse(formString)
+      console.log("objeto a ser inserido", form)
+
+      this.setState({
+
+      tarefas: this.state.form
+
+      });
+
+    }
+
+  };
+
+  componentDidUpdate() {
+
+    const dadosArmazenados = {
+
+      ...this.state.tarefas
+
+    };
+
+    console.log(dadosArmazenados)
+
+    window.localStorage.setItem("dadosArmazenados", JSON.stringify(dadosArmazenados))
+
   }
 
   render() {
+
+    console.log("teste",this.state)
+
     const listaFiltrada = this.state.tarefas.filter(tarefa => {
       switch (this.state.filtro) {
         case 'pendentes':
@@ -76,8 +148,14 @@ class App extends React.Component {
       <div className="App">
         <h1>Lista de tarefas</h1>
         <InputsContainer>
-          <input value={this.state.inputValue} onChange={this.onChangeInput}/>
-          <button onClick={this.criaTarefa}>Adicionar</button>
+          <input 
+            value={this.state.inputValue} 
+            onChange={this.onChangeInput}
+          />
+
+          <button 
+          onClick={this.componentDidUpdate}
+          onClick={this.criaTarefa}>Adicionar</button>
         </InputsContainer>
         <br/>
 
@@ -95,6 +173,7 @@ class App extends React.Component {
               <Tarefa
                 completa={tarefa.completa}
                 onClick={() => this.selectTarefa(tarefa.id)}
+                
               >
                 {tarefa.texto}
               </Tarefa>
